d4cc6c35032b009b9551908759903fb63b33dc06
Chechko Ivan <ci@skyname.net>
2019-10-29 17:31:51 +0300

    add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,11 +17,26 @@ export default {
       }
     ],
     link: [
+      { rel: 'apple-touch-icon', href: '/favicon/apple-touch-icon.png' },
       {
         rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      }
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png'
+      },
+      {
+        rel: 'mask-icon',
+        color: '#5bbad5',
+        href: '/favicon/safari-pinned-tab.svg'
+      },
+      { rel: 'msapplication-TileColor', content: '#2d89ef' },
+      { rel: 'theme-color', content: '#ffffff' }
     ]
   },
   loading: {
